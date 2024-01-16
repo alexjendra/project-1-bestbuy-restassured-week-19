d1fd5393d86d93e2fc8e13ad4e50e6af1dfa948d
alexjendra <khushbue51@gmail.com>
2024-01-16 23:41:55 +0000

    my homework

---
+++ src/test/java/com/bestbuy/crudtest/ProductsCRUDTest.java
@@ -0,0 +1,17 @@
+package com.bestbuy.crudtest;
+
+import com.bestbuy.testbase.TestBaseClass;
+
+public class ProductsCRUDTest extends TestBaseClass {
+    static String name = "Duracell - AAA Batteries (8-Pack)";
+    static String type = "HardGood";
+    static double price = 21.99;
+    static int shipping = 2;
+    static String upc = "052334424008";
+    static String description = "Compatible with select electronic devices; AAA size; DURALOCK Power Preserve technology; 8-pack";
+    static String manufacturer = "Duracell";
+    static String model = "HN2400C4B";
+
+    static int productId;
+
+}
